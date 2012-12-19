eb97d5f642193f7915a31e3605931484096296d7
Whitexp <lp_gustavo_@hotmail.com>
2012-12-19 12:59:51 -0200

    ficado about.php

--- about.php
+++ about.php
@@ -14,10 +14,10 @@ a integração e pesquisa de produtos novos/antigos, e logicamente, avaliar os m
 ';
 $lang['TAAVATAR'] = 'Como eu troco minha imagem de avatar?';
 $lang['AAVATAR'] = '
-<p>No momento a troca de images do seu avatar não esta disponivel, estamos trabalhando na implementação dessa característica</p>';
+<p>No momento a troca de imagens do seu avatar não esta disponível, estamos trabalhando na implementação dessa característica</p>';
 $lang['TAJOB'] = 'Como eu posso trabalhar no WadUthink?';
 $lang['AJOB'] = '
-<p>Estamos constantemente procurando pessoas para trabalha no wadUthink, caso esteja interessado, enviar um email para<a href="mailto:jobs@waduthink.com">jobs@waduthink.com</a>
+<p>Estamos constantemente procurando pessoas para trabalha no wadUthink, caso esteja interessado, enviar um e-mail para<a href="mailto:jobs@waduthink.com">jobs@waduthink.com</a>
 </p>';
 $lang['THOWREVIEW'] = 'Como eu faço uma avaliação?';
 $lang['HOWREVIEW'] = '
@@ -27,25 +27,14 @@ Depois disso, você pode criar uma avaliação nova na pagina de avaliações, b
 ';
 $lang['TAFEATURE'] = 'Eu posso enviar feedback ou ideias, opiniões e sugestões?';
 $lang['AFEATURE'] = '
-<p>Claro, estamos constantemente procurando maneiras de melhorar o wadUthink, basta nos enviar um email para <a href="mailto:feedback@waduthink.com">
+<p>Claro, estamos constantemente procurando maneiras de melhorar o wadUthink, basta nos enviar um e-mail para <a href="mailto:feedback@waduthink.com">
 feedback@waduthink.com</a>		
 		</p>';
-$lang['TALANGUAGE'] = 'Eu posso solicitar incluir uma nova lingua?';
+$lang['TALANGUAGE'] = 'Eu posso solicitar a inclusão de uma nova língua?';
 $lang['ALANGUAGE'] = '
-<p>Claro, estamos constantemente procurando maneiras de melhorar o wadUthink, basta nos enviar um email para <a href="mailto:feedback@waduthink.com">
+<p>Claro, estamos constantemente procurando maneiras de melhorar o wadUthink, basta nos enviar um e-mail para <a href="mailto:feedback@waduthink.com">
 feedback@waduthink.com</a>		
 		</p>';
 $lang['TAEXIST'] = 'O produto/local que eu quero avaliar não existe, o que fazer?';
 $lang['AEXIST'] = '
-<p>Se o produto ou local que você quer avaliar não existe, por favor digitar no campo de produto/local da pagina de avaliação o nome do produto,
-o mesmo será adicionado automaticamente no nosso banco de dados. Iremos colocar o produto na categoria correta e enviar as suas imagens.		
-</p>';
-$lang['TAIMAGE'] = 'Como eu envio a imagem do meu produto/local?';
-$lang['AIMAGE'] = '
-<p>No momento somente os funcionarios do wadUthink podem enviar imagens de produtos/locais para evitar envio de imagens protegidas e "trolling". 
-</p>';
-$lang['TAEDIT'] = 'Como eu edito as minhas avaliações?';
-$lang['AEDIT'] = '
-<p>Para editar as avaliações, entre na area de <a href="https://waduthink.com/mypage#reviews">avaliações</a> na <a href="https://waduthink.com/mypage">Minha Pagina</a>
-</p>';
-?>
+
