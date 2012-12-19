0756059f8a54c90584aec081c3ec4c012231e084
Whitexp <lp_gustavo_@hotmail.com>
2012-12-19 13:13:14 -0200

    fixes for registercheck.php

--- registercheck.php
+++ registercheck.php
@@ -6,8 +6,9 @@ Language: Portugues Brasil
 */
 $lang['EXISTS'] = 'Usuário já existe';
 $lang['PASSMATCH'] = 'As senhas não conferem';
-$lang['TERMS'] = 'Voce precisa aceitar as condicoes e termos';
-$lang['EMATCH'] = 'Emails não conferem';
-$lang['INVALIDEMAIL'] = 'Email invalido';
-$lang['DONE'] = 'Registro concluído, divirtase!';
+$lang['TERMS'] = 'Você precisa aceitar as condições e termos';
+$lang['EMATCH'] = 'E-mails não conferem';
+$lang['INVALIDEMAIL'] = 'E-mail invalido';
+$lang['DONE'] = 'Registro concluído, divirta-se!';
 ?>
+
